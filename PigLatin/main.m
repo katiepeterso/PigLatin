f679a2ca9b4a1eb4ffa916edc21657627848dda3
katiepeterso <katiepeterso@outlook.com>
2015-08-28 13:08:47 -0700

    initial commit

---
+++ PigLatin/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  PigLatin
+//
+//  Created by Katherine Peterson on 2015-08-28.
+//  Copyright (c) 2015 KatieExpatriated. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
